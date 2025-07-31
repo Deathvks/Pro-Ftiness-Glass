79e7b0718cdeda316f15344a30722afbef1a4005
Deathvks <dylanjesussuarez@gmail.com>
2025-07-31 21:07:15 +0000

    más cambios

--- backend/db.js
+++ backend/db.js
@@ -3,12 +3,13 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+// Esta es la configuración para producción, usando variables individuales
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST,
     dialect: 'mysql'
   }
 );
