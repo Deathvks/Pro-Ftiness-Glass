d7f1db6e6988eff3a1a901aa4758b11bbd1de864
Deathvks <dylanjesussuarez@gmail.com>
2025-07-31 21:15:24 +0000

    espero el final de este rollo

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// Esta es la configuración para producción, usando variables individuales
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
