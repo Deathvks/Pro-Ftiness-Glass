09e6ec18a8b0c6092a0db23b2c61258411a6ad0d
Deathvks <dylanjesussuarez@gmail.com>
2025-08-01 01:56:35 +0000

    fix: Force production env for db scripts

--- backend/db.js
+++ backend/db.js
@@ -3,13 +3,23 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  // Usa la variable de producción si existe, si no, la de desarrollo
+  isProduction ? process.env.MYSQL_DATABASE : process.env.DB_NAME,
+  isProduction ? process.env.MYSQL_USERNAME : process.env.DB_USER,
+  isProduction ? process.env.MYSQL_PASSWORD : process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST,
-    dialect: 'mysql'
+    host: isProduction ? process.env.MYSQL_HOST : process.env.DB_HOST,
+    dialect: 'mysql',
+    // Aplica SSL solo en producción
+    dialectOptions: isProduction ? {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    } : {}
   }
 );
 
