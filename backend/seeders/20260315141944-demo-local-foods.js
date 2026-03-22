93b9335e945b18409c82e8937069ceeb2446453d
Deathvks <dylanjesussuarez@gmail.com>
2026-03-22 16:07:07 +0000

    arreglado el seeder

--- backend/seeders/20260315141944-demo-local-foods.js
+++ backend/seeders/20260315141944-demo-local-foods.js
@@ -63,13 +63,9 @@ export default {
     console.log(`✅ ¡Proceso terminado! Insertando ${uniqueFoods.length} alimentos únicos de España en la base de datos...`);
 
     if (uniqueFoods.length > 0) {
-<<<<<<< HEAD
-      await queryInterface.bulkInsert('local_foods', uniqueFoods);
-=======
       await queryInterface.bulkInsert('local_foods', uniqueFoods, {
         ignoreDuplicates: true // <--- AÑADE ESTA LÍNEA
       });
->>>>>>> main
     }
   },
 
