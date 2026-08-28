c645b9cdea915cca099c1263e93fa24fe3df65b3
Deathvks <dylanjesussuarez@gmail.com>
2026-08-28 19:45:51 +0100

    chore: remove miscellaneous build artifacts and scratch files from git tracking

--- scratch_sync.js
+++
@@ -1,9 +0,0 @@
-import sequelize from './backend/db.js';
-import models from './backend/models/index.js';
-
-async function sync() {
-    await sequelize.sync({ alter: true });
-    console.log("DB Synced");
-    process.exit(0);
-}
-sync();
