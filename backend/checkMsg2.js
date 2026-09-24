f9a01af8662dba1f04d86d4b6a8cdfd78f4795b0
Deathvks <dylanjesussuarez@gmail.com>
2026-09-24 07:14:18 +0100

    chore: remove temporary patch and bot scripts

--- backend/checkMsg2.js
+++
@@ -1,9 +0,0 @@
-import models from './models/index.js';
-import { Op } from 'sequelize';
-async function run() {
-  const msgs = await models.Message.findAll({ where: { content: { [Op.like]: '%compromiso%' } } });
-  console.log('Found:', msgs.length);
-  msgs.forEach(m => console.log(m.content));
-  process.exit(0);
-}
-run();
